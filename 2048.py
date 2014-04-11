847f2f7c83d052c961dc4209acceb42df4ebacd5
homich77 <homich77@gmail.com>
2014-04-11 12:08:18 +0300

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,4 +1,4 @@
-from struct_improved import size, generate_board, print_board, move
+from struct_improved import Struct2048
 from optparse import OptionParser
 #import argparse
 
@@ -6,19 +6,19 @@ parserO = OptionParser()
 parserO.add_option("-c", '--continue', dest="iscontinue", type='int', default='0', help='Continue game')
 parserO.add_option("-s", '--size', dest="size", type='int', default='4', help='Size board')
 parserO.add_option("-n", '--new', dest="new_game", type='int', default='0', help='Start new game')
+parserO.add_option("-e", '--empty', dest="empty", type='str', default='0', help='Value of empty fields')
 (options, args) = parserO.parse_args()
-
-if options.size != size:
-    size = options.size
-print size
-generate_board()
-
-print_board()
 '''
 parserA = argparse.ArgumentParser()
 parserA.add_argument('-n', action='store', dest='n', type='int', help='Simple value n', default='False')
 parserA.add_argument('-k', action='store', dest='k', type='int', help='Simple value k', default='True')
 '''
+
+game = Struct2048(options.size, options.empty)
+
+game.generate_board()
+game.print_board()
+
 '''
 
 isActive = True
